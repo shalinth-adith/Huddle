2554f692a90295e571b518fafa0799fa1e956ebd
shalinth-adith <shalinth.adith@outlook.com>
2025-11-19 10:33:23 +0530

    Initial Commit

---
+++ Huddle/HuddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuddleApp.swift
+//  Huddle
+//
+//  Created by shalinth adithyan on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HuddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HuddleTests/HuddleTests.swift
@@ -0,0 +1,17 @@
+//
+//  HuddleTests.swift
+//  HuddleTests
+//
+//  Created by shalinth adithyan on 19/11/25.
+//
+
+import Testing
+@testable import Huddle
+
+struct HuddleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
