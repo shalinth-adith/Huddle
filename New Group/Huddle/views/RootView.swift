97ddc7b3b83930a4fced9fc6dd79e0fb4ae94444
shalinth-adith <shalinth.adith@outlook.com>
2025-11-19 15:49:58 +0530

    Add placeholder view files for onboarding flow
    - Create RootView for navigation logic
    - Create WelcomeView for initial welcome screen
    - Create NameInputView for user name input
    - Set up structure for authentication UI flow

---
+++ New Group/Huddle/views/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Huddle
+//
+//  Created by shalinth adithyan on 19/11/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
